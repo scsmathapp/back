dbbe9d9b63be36de261b5f14c2f4c564e9fdddd7
Suyash FOWDAR <suyashkrishnadas@gmail.com>
2024-04-05 10:10:38 +0400

    Update to 2024

--- src/controller/city.controller.js
+++ src/controller/city.controller.js
@@ -2,116 +2,134 @@ import https from 'https';
 import async from 'async';
 import fs from 'fs';
 import _ from 'lodash';
-import { finished } from 'stream';
+import {finished} from 'stream';
 import cities from '../assets/cities.js';
 
 _.each(cities, city => {
-	console.log('Reading city ' + city.name);
-	fs.readFile('../assets/json/' + city.slug + '_538.json', 'utf8', (err, location) => {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log('Casting ' + city.name);
-			writeClient(city, standardizeData(JSON.parse(location)));
-		}
-	})
+    console.log('Reading city ' + city.name);
+    fs.readFile('D:/Dev/Webstorm/SCSMath/back/src/assets/json/' + city.slug + '_539.json', 'utf8', (err, location) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Casting ' + city.name);
+            writeClient(city, standardizeData(JSON.parse(location)));
+        }
+    })
 });
 
-	const standardizeData = (calDates) => {
-		let newCountry = {};
-		
-		_.each(calDates, (calDate, dateText) => {
-			let day = {
-				'lunar-day': calDate['en-masa-title'] + ' ' + calDate['en-tithi-title'],
-				events: []
-			}
+// export default function (req, res) {
+//     fetch('https://vaishnavacalendar.org/ics/new_york_539_en.txt')
+//         .then(res => res.text())
+//         .then(txt => {
+//             const icalEvents = txt.split('BEGIN:VEVENT');
+//             const dateMap = {};
+//             let splitTxt = '';
+//
+//             _.each(icalEvents, function (icalEvent) {
+//                 icalEvent = icalEvent.split('DTSTART;VALUE=DATE:')[1];
+//                 splitTxt = icalEvent.split('\\r\\nSUMMARY:');
+//                 dateMap[splitTxt[0]] = splitTxt[splitTxt.length - 1];
+//             });
+//
+//             res.send();
+//         });
+// }
 
-			castEvents(day, calDate);
+const standardizeData = (calDates) => {
+    let newCountry = {};
 
-			if (day.ekadashi || day.special || (day.events && day.events.length)) {
-				newCountry[dateText] = day;
-			}
-		});
+    _.each(calDates, (calDate, dateText) => {
+        let day = {
+            'lunar-day': calDate['en-masa-title'] + ' ' + calDate['en-tithi-title'],
+            events: []
+        }
 
-		return ('export default ' + JSON.stringify(newCountry));
-	}
+        castEvents(day, calDate);
 
-	const writeClient = (city, data) => {
-		console.log('Writing client ' + city.name);
-		fs.writeFile('/home/suyash/Documents/Projects/Math/client/public/assets/cal/location-' + city.city_id + '.js', data, (err) => {
-			if (err) {
-				console.log(err);
-			}
-		});
-	}
+        if (day.ekadashi || day.special || (day.events && day.events.length)) {
+            newCountry[dateText] = day;
+        }
+    });
 
-	const castEvents = (day, date) => {
-		let eventImages = {
-			AcharyaMhj: 'Nirmal Acharya',
-			GovindaMhj: 'Sundar Govinda',
-			SwamiMhj: 'Bhaktivedanta',
-			SridharMhj: 'Raksak Sridhar',
-			SaraswatiMhj: 'Siddhanta Saraswati',
-			GauraKisoraMhj: 'Paramahamsa Sri Srila Gaura Kishor',
-			BhaktivinodaThakur: 'Sachchidananda Bhakti Vinod',
-			JagannathMhj: 'Srila Jagannath Das Babaji',
-			SriBalaram: 'Sri Baladev',
-			SriKrishna: 'Janmashtami',
-			SriRadha: 'Radhashtami',
-			SriGaura: 'Gaura Purnima',
-			SriNitai: 'Sri Nityananda Prabhu'
-		}, eventStr = date['en-line'];
+    return ('export default ' + JSON.stringify(newCountry));
+}
 
-		if (eventStr) {
-			eventStr = eventStr.replace(/(\. )/g, '---');
-			eventStr = eventStr.replace(/(\<b\>)|(\<\/b\>)/g, '');
-			
-			let splitEvents = eventStr.split('---');
-			
-			_.eachRight(splitEvents, (ev, evIndex) => {
-				let newEvent = {};
-				if (ev) {
-					if (ev.includes('Ekadashi') || ev.includes('Mahadvadashi')) {
-						day.ekadashi = ev;
-						splitEvents.splice(evIndex, 1);
-						return false;
-					}
-					
-					if (ev.includes('No fast') || ev.toLowerCase().includes('paran')) {
-						day.special = ev;
-						splitEvents.splice(evIndex, 1);
-						return false;
-					}
-	
-					if (ev.includes('<a href="')) {
-						let array = ev.split('<a href="');
-						let array2 = array[1].split('\"');
-						
-						if (array2[1]) {
-							array2[1] = array2[1].split('>')[1];
-							newEvent.name = array[0] + array2[1];
-						} else {
-							newEvent.name = array[0];
-						}
-						
-						newEvent.link = array2[0];
-					} else {
-						newEvent.name = ev;
-					}
-					
-					newEvent.name = newEvent.name.replace(/(\<i\>)|(\<\/i\>)|(\<\/a\>)|(\<\/a)/g, '');
-					
-					_.each(eventImages, (lord, img) => {
-						if (newEvent.name.includes(lord)) {
-							newEvent.url = 'url(../assets/img/' + img + '.jpg)';
-						}
-					});
+const writeClient = (city, data) => {
+    console.log('Writing client ' + city.name);
+    fs.writeFile('D:/Dev/Webstorm/SCSMath/client/public/assets/cal/location-' + city.city_id + '.js', data, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
 
-					if (!_.isEmpty(newEvent)) {
-						day.events.push(newEvent);
-					}
-				}
-			});
-		}
+const castEvents = (day, date) => {
+    let eventImages = {
+        AcharyaMhj: 'Nirmal Acharya',
+        GovindaMhj: 'Sundar Govinda',
+        SwamiMhj: 'Bhaktivedanta',
+        SridharMhj: 'Raksak Sridhar',
+        SaraswatiMhj: 'Siddhanta Saraswati',
+        GauraKisoraMhj: 'Paramahamsa Sri Srila Gaura Kishor',
+        BhaktivinodaThakur: 'Sachchidananda Bhakti Vinod',
+        JagannathMhj: 'Srila Jagannath Das Babaji',
+        SriBalaram: 'Sri Baladev',
+        SriKrishna: 'Janmashtami',
+        SriRadha: 'Radhashtami',
+        SriGaura: 'Gaura Purnima',
+        SriNitai: 'Sri Nityananda Prabhu'
+    }, eventStr = date['en-line'];
 
-	}
+    if (eventStr) {
+        eventStr = eventStr.replace(/(\. )/g, '---');
+        eventStr = eventStr.replace(/(\<b\>)|(\<\/b\>)/g, '');
+
+        let splitEvents = eventStr.split('---');
+
+        _.eachRight(splitEvents, (ev, evIndex) => {
+            let newEvent = {};
+            if (ev) {
+                if (ev.includes('Ekadashi') || ev.includes('Mahadvadashi')) {
+                    day.ekadashi = ev;
+                    splitEvents.splice(evIndex, 1);
+                    return false;
+                }
+
+                if (ev.includes('No fast') || ev.toLowerCase().includes('paran')) {
+                    day.special = ev;
+                    splitEvents.splice(evIndex, 1);
+                    return false;
+                }
+
+                if (ev.includes('<a href="')) {
+                    let array = ev.split('<a href="');
+                    let array2 = array[1].split('\"');
+
+                    if (array2[1]) {
+                        array2[1] = array2[1].split('>')[1];
+                        newEvent.name = array[0] + array2[1];
+                    } else {
+                        newEvent.name = array[0];
+                    }
+
+                    newEvent.link = array2[0];
+                } else {
+                    newEvent.name = ev;
+                }
+
+                newEvent.name = newEvent.name.replace(/(\<i\>)|(\<\/i\>)|(\<\/a\>)|(\<\/a)/g, '');
+
+                _.each(eventImages, (lord, img) => {
+                    if (newEvent.name.includes(lord)) {
+                        newEvent.url = 'url(../assets/img/' + img + '.jpg)';
+                    }
+                });
+
+                if (!_.isEmpty(newEvent)) {
+                    day.events.push(newEvent);
+                }
+            }
+        });
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import express from 'express';
-import bodyparser from 'body-parser';
-import mongoose from 'mongoose';
-import mrq from 'mongoose-rest-query';
 import { Server } from 'http';
-import morgan from 'morgan';
 import fs from 'fs';
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
 import config from './app.config.js';
 import { cors } from './middleware/index.js';
 import cityCtrl from './controller/city.controller.js';
@@ -14,25 +8,20 @@ import cityCtrl from './controller/city.controller.js';
 const app = express(),
 			http = Server(app);
 
-
-mongoose.Promise = global.Promise;
-
-app.use(morgan('tiny'));
-
 // app.use(mrq.db);
 app.use(cors);
 // mrq.config.modelSchemas = require('./model');
 
-app.use(bodyparser.json({
-	limit: '100mb'
-}));
-
-app.use(bodyparser.raw({
-	type: 'binary/octet-stream',
-	limit: '10mb'
-}));
-
-app.use(express.static('public'));
+// app.use(bodyparser.json({
+// 	limit: '100mb'
+// }));
+//
+// app.use(bodyparser.raw({
+// 	type: 'binary/octet-stream',
+// 	limit: '10mb'
+// }));
+//
+// app.use(express.static('public'));
 
 // const restify = mrq.restify;
 
@@ -44,7 +33,7 @@ app.use(express.static('public'));
 // app.post('/ctrl/login', require('./controller/login.controller').login);
 
 // app.post('/ctrl/addcity', require('./controller/city.controller'));
-app.post('/ctrl/city/:id', cityCtrl);
+app.get('/ctrl/city', cityCtrl);
 app.get('/cities', (req, res) => {
 	import('./assets/cities.js').then((cities) => {
 		res.send(cities);
@@ -54,109 +43,6 @@ app.get('/cities', (req, res) => {
 app.get('/', function(req, res) {
 	console.log('Here we go!');
 	res.send({});
-})
-
-app.get('/list', (req, res) => {
-	fs.readFile('/home/suyash/Documents/Projects/Math/back/src/temp.txt', function(err, buf) {
-		if (err) {
-			res.status(404).send(err);
-		} else {
-			res.send(JSON.parse(buf.toString()));
-		}
-	});
-});
-
-app.get('/count/:id', (req, res) => {
-	fs.readFile('/home/suyash/Documents/Projects/Math/back/src/temp.txt', function(err, buf) {
-		if (err) {
-			res.status(404).send(err);
-		} else {
-			const path = '/home/suyash/Downloads/Wedding/';
-	
-			fs.readdir(path, (err, files) => {
-				if (err || !files[req.params.id]) {
-					res.status(404).send(err || 'Not found!');
-				} else {
-					let list = JSON.parse(buf.toString());
-					if (list.dulha.indexOf(files[req.params.id]) >= 0) {
-						list.dulhaSelected = true;
-					}
-					if (list.dulhan.indexOf(files[req.params.id]) >= 0) {
-						list.dulhanSelected = true;
-					}
-					list.dulha = list.dulha.length;
-					list.dulhan = list.dulhan.length;
-					res.send(list);
-				}
-			});
-		}
-	});
-});
-
-app.get('/wedding/:id', (req, res) => {
-	const path = '/home/suyash/Downloads/Wedding/';
-	
-	fs.readdir(path, (err, files) => {
-		if (err || !files[req.params.id]) {
-			res.status(404).send(err || 'Not found!');
-		} else {
-			const s = fs.createReadStream(path + files[req.params.id]);
-			
-			s.on('open', () => {
-				res.set('Content-Type', 'image/jpg');
-				s.pipe(res);
-			});
-		
-			s.on('error', (err) => {
-					res.set('Content-Type', 'text/plain');
-					res.status(404).send(err);
-			});
-		}
-	});
-});
-
-app.get('/select/:dulha/:id', (req, res) => {
-	let url = '/home/suyash/Documents/Projects/Math/back/src/temp.txt';
-	fs.readFile(url, function(err, buf) {
-		if (err) {
-			res.status(404).send(err);
-		} else {
-			let person = req.params.dulha == 1 ? 'dulha' : 'dulhan';
-			let list = JSON.parse(buf.toString());
-			const path = '/home/suyash/Downloads/Wedding/';
-			// res.send({});
-			fs.readdir(path, (err, files) => {
-				if (err || !files[req.params.id]) {
-					res.status(404).send(err || 'Not found!');
-				} else {
-					let index = list[person].indexOf(files[req.params.id]);
-					if (index < 0) {
-						list[person].push(files[req.params.id]);
-					} else {
-						list[person].splice(index, 1);
-					}
-					fs.writeFile(url, JSON.stringify(list), (err) => {
-						if (err) {
-							res.status(500).send(err);
-						} else {
-							res.send({});
-						}
-					});
-				}
-			});
-		}
-	});
-});
-
-app.get('/getIndex/:name', (req, res) => {
-	const path = '/home/suyash/Downloads/Wedding/';
-		fs.readdir(path, (err, files) => {
-			if (err) {
-				res.status(404).send(err);
-			} else {
-				res.send({ id: files.indexOf(req.params.name) });
-			}
-		});
 })
 
 const server = app.listen(config.port, function() {
